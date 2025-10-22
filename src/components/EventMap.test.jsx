c0d7e8a539853689e0cbaaa233bc1039de2a83ea
Tomplan <34377034+Tomplan@users.noreply.github.com>
2025-10-22 15:53:33 +0200

    Mock IntersectionObserver in tests; verify orange SVG marker icons for all marker types

--- src/components/EventMap.test.jsx
+++ src/components/EventMap.test.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import EventMap, { createBoothMarkerIcon, createSpecialMarkerIcon } from './EventMap';
+
+// Mock IntersectionObserver for test environment
+beforeAll(() => {
+  global.IntersectionObserver = class {
+    constructor() {}
+    observe() {}
+    disconnect() {}
+  };
+});
 import L from 'leaflet';
 
 describe('EventMap marker icons', () => {
