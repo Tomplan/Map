3f0018dc2b11e2b38536375b8c3cd0f78e80954a
Tomplan <34377034+Tomplan@users.noreply.github.com>
2025-10-24 22:19:32 +0200

    Update markerIcons.js with latest changes

--- src/utils/markerIcons.js
+++ src/utils/markerIcons.js
@@ -2,15 +2,21 @@ import L from 'leaflet';
 import '../libs/Leaflet.Icon.Glyph.js';
 import orangeIconUrl from '../../assets/icons/glyph-marker-icon-blue.svg';
 
-export function createMarkerIcon({ glyph = 'home', glyphColor = 'white', bgColor = 'white', glyphSize = '11px', iconUrl = '/assets/icons/glyph-marker-icon-green.svg', className }) {
+export function createMarkerIcon({ glyph, glyphColor = 'white', bgColor = 'white', glyphSize = '11px', iconUrl = '/assets/icons/glyph-marker-icon-blue.svg', className, iconSize, prefix }) {
   return L.icon.glyph({
-    prefix: 'mdi', // Material Design Icons
-    glyph,
-    glyphColor,
+    iconUrl: iconUrl,
+    iconSize: iconSize || [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [0, 0],
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12, 41],
+    prefix: prefix || '', // Material Design Icons
+    glyph: glyph || '',
+    glyphColor: glyphColor || 'white',
     bgColor,
-    glyphSize,
-    iconUrl,
-    iconSize: [25, 41],
+    glyphSize: [35, 35],
     className
   });
 }
