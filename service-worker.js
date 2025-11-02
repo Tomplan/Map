d2a39cd6afd5f74f032147d01df1c67996cd7157
Tomplan <34377034+Tomplan@users.noreply.github.com>
2025-11-02 12:15:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@ self.addEventListener('activate', () => {
 // Cache Carto Voyager map tiles
 self.addEventListener('fetch', event => {
   const url = event.request.url;
+  // Cache Carto Voyager tiles
   if (url.includes('cartodb-basemaps')) {
     event.respondWith(
       caches.open('map-tiles').then(cache => {
@@ -21,5 +22,34 @@ self.addEventListener('fetch', event => {
         });
       })
     );
+    return;
+  }
+  // Cache Esri World Imagery tiles
+  if (url.includes('arcgisonline.com/ArcGIS/rest/services/World_Imagery')) {
+    event.respondWith(
+      caches.open('map-tiles').then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request).then(networkResponse => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+    return;
+  }
+  // Cache marker icons and logos
+  if (url.match(/\/assets\/(icons|logos)\//)) {
+    event.respondWith(
+      caches.open('map-assets').then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request).then(networkResponse => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+    return;
   }
 });
